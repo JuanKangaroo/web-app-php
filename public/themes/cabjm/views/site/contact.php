c752f72965df0ab25fb8252207f23c0b523dc79c
Valentin Ursuleac <ursuleacv@gmail.com>
2017-06-26 10:14:48 -0400

    contact and verify page content

--- public/themes/cabjm/views/site/contact.php
+++ public/themes/cabjm/views/site/contact.php
@@ -1,44 +1,12 @@
-<div class="container container-landing contact p-5">
+<div class="container container-landing landing-text-box p-5">
     <h3>Contact Us</h3>
-    <p class="lead">
-        Wilsons is a leading fuel supplier in Atlantic Canada. We strive to provide a premium level of customer service and high value products to our customers.
-    </p>
-    <p class="lead">
-        Along with our home comfort services, retail gas stations and convenience stores we offer a full range of residential and commercial security solutions.
+
+    <p>
+        We are eager to hear from you! Give us a call, or send us an email
     </p>
 
-    <h4 style="color: red;">Main Number 1-877-570-4040</h4>
+    <p class="lead">+1 876 978 5508</p>
+    <p class="lead">info@cabjm.com</p>
 
-    <br>
-
-    <div class="row">
-        <div class="col-12 col-md-4">
-            <address>
-                <strong>Halifax Office</strong> <br>
-                473 Cobequid Road <br>
-                Sackville, NS B4C 4E9 <br>
-
-                <strong>902-429-4545</strong>
-            </address>
-        </div>
-        <div class="col-12 col-md-4">
-            <address>
-                <strong>Truro Office</strong> <br>
-                97 Pleasant Street, <br>
-                Truro, NS B2N 5G1 <br>
-
-                <strong>902-895-4429</strong>
-            </address>
-        </div>
-        <div class="col-12 col-md-4">
-            <address>
-                <strong>Bridgewater Office</strong> <br>
-                2984 Highway 325 <br>
-                Wileville, NS B4V 5G5 <br>
-
-                <strong>902-527-0009</strong>
-            </address>
-        </div>
-    </div>
-    
+    <address>94D Old Hope Road, Kgn 6. St. Andrew, Jamaica W.I </address>
 </div>
--- public/themes/cabjm/views/site/verify.php
+++ public/themes/cabjm/views/site/verify.php
@@ -2,79 +2,23 @@
     <input type="hidden" name="verify_token" value="<?= $token ?>" >
     <input type="hidden" name="verify_email" value="<?= $email ?>" >
 
-    <!-- <div class="row">
-        <div class="col-12 col-md-6 offset-md-3">
-            <div class="alert alert-success alert-dismissible fade show" role="alert" style="display: none;">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <div id="message"></div>
-            </div>
-        </div>
-    </div> -->
-
     <div id="profile_subaccounts_form" class="mb-5" style="display: none;">
         <div class="row">
-            <div class="col-12 col-md-6 offset-md-3">
-                <div class="alert alert-info" role="alert">
+            <div class="col-12 col-md-6 offset-md-3 landing-text-box py-5">
+                <div class="alert" role="alert">
                     <h4 class="alert-heading">Welcome <?= $email ?></h4>
-                    <p>Wilsons is a leading fuel supplier in Atlantic Canada. 
-                    We strive to provide a premium level of customer service and high value products to our customers.</p>
-                    <p class="mb-0">Along with our home comfort services, retail gas stations and convenience stores we 
-                    offer a full range of residential and commercial security solutions.</p>
-                </div>
-
-                <div class="alert alert-warning" role="alert" id="verify_email_not_veified" style="display: none;">
-                    <h4 class="alert-heading">Your email is not verified.</h4>
-                    <p>Click the verification link to verify your email.</p>
-                </div>
-
-                <div class="text-center">
-                    <h4 class="mb-3">Link Account</h4>
-
-                    <button class="btn btn-outline-primary js-add-pos-account__btn" 
-                        data-pos-id="infosys" data-pos-name="Home Heat">
-                        Home Heat
-                    </button>
-                    <button class="btn btn-outline-primary js-add-pos-account__btn" 
-                        data-pos-id="sedona" data-pos-name="Security">
-                        Security
-                    </button>
+                    <p>
+                        Caribbean Assurance Brokers Ltd. (CAB), a name synonymous with innovation, 
+                        is a multi-line insurance brokerage offering the full spectrum of insurance products 
+                        and services throughout Jamaica. Through these offerings, we provide our clients 
+                        with peace of mind in preparation for life’s uncertainties.
+                    </p>
                 </div>
 
                 <div class="text-center mt-3">
-                    <a href="/home" class="btn btn-outline-info">Go to my Account</a>
+                    <a href="/home" class="btn btn-primary">Go to my Account</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-<div class="modal fade" id="addAccountsModal" tabindex="-1" role="dialog" aria-labelledby="addAccountsModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="addAccountsModalLabel">Add your Accounts</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form class="form" id="add_pos_account__form">
-                    <input type="hidden" name="pos_id">
-                    <div class="form-group">
-                        <input type="text" name="account_id" class="form-control form-control-lg" value="" placeholder="Account ID" autofocus="">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="postal_code" class="form-control form-control-lg" value="" placeholder="Postal Code">
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer align-self-center">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary js-add-pos-account__confirm-btn">Add Account</button>
-            </div>
-        </div>
-    </div>
-</div>
-
