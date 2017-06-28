8ba7ecb20281fdccb722cb624a78d6714ca0a1b3
Valentin Ursuleac <ursuleacv@gmail.com>
2017-06-28 16:35:51 -0400

    fixing contact page

--- public/themes/cabjm/views/site/_contact.php
+++ public/themes/cabjm/views/site/_contact.php
@@ -1,37 +1,12 @@
 <div class="container-fluid">
 
-    <h4 style="color: red;">Main Number 1-877-570-4040</h4>
+    <p>We are eager to hear from you! Give us a call, or send us an email </p>
 
-    <br>
+    <p class="lead"><i class="fa fa-phone fa-fw"></i> +1 876 978 5508</p>
+    <p class="lead"><i class="fa fa-envelope fa-fw"></i> info@cabjm.com</p>
 
-    <div class="row">
-        <div class="col-12 col-md-4">
-            <address>
-                <strong>Halifax Office</strong> <br>
-                473 Cobequid Road <br>
-                Sackville, NS B4C 4E9 <br>
-
-                <strong>902-429-4545</strong>
-            </address>
-        </div>
-        <div class="col-12 col-md-4">
-            <address>
-                <strong>Truro Office</strong> <br>
-                97 Pleasant Street, <br>
-                Truro, NS B2N 5G1 <br>
-
-                <strong>902-895-4429</strong>
-            </address>
-        </div>
-        <div class="col-12 col-md-4">
-            <address>
-                <strong>Bridgewater Office</strong> <br>
-                2984 Highway 325 <br>
-                Wileville, NS B4V 5G5 <br>
-
-                <strong>902-527-0009</strong>
-            </address>
-        </div>
-    </div>
-    
+    <address>
+        <i class="fa fa-map-marker fa-fw"></i> 
+        94D Old Hope Road, Kgn 6. St. Andrew, Jamaica W.I 
+    </address>
 </div>
